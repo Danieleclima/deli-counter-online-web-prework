0e3f68278b0956ccd6444083555a2cd3c9bdc098
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-03-21 19:41:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,6 @@ def take_a_number (katz_deli, name)
   katz_deli << name
     position = katz_deli.size
     puts "Welcome, #{name}. You are number #{position} in line."
-  end
 end
 
 def now_serving (katz_deli)
